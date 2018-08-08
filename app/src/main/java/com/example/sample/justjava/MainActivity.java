d578e68e97fa9f5c41a4a4c9c8f1177c7a703587
madhank93 <maddymadhan946@gmail.com>
2018-08-09 05:11:19 +0530

    Initial commit

--- app/src/main/java/com/example/sample/justjava/MainActivity.java
+++ app/src/main/java/com/example/sample/justjava/MainActivity.java
@@ -3,64 +3,95 @@ package com.example.sample.justjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int initialCoffeeQuanity = 2;
-    private int oneCoffeeCupPrice = 5;
+	private int initialCoffeeQuantity = 2;
+	private int oneCoffeeCupPrice = 5;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    /**
-     * This method is called when the increment button is clicked.
-     */
+	/**
+	 * This method is called when the increment button is clicked.
+	 */
+	public void increment(View view) {
+		initialCoffeeQuantity = initialCoffeeQuantity + 1;
+		displayQuanity(initialCoffeeQuantity);
+	}
 
-    public void increment(View view) {
-        initialCoffeeQuanity = initialCoffeeQuanity + 1;
-        displayQuanity(initialCoffeeQuanity);
-    }
+	/**
+	 * This method is called when the decrement button is clicked.
+	 */
+	public void decrement(View view) {
+		if (initialCoffeeQuantity > 1) {
+			initialCoffeeQuantity = initialCoffeeQuantity - 1;
+			displayQuanity(initialCoffeeQuantity);
+		}
+	}
 
-    /**
-     * This method is called when the decrement button is clicked.
-     */
+	/**
+	 * This method is called when the order button is clicked.
+	 */
+	public void submitOrder(View view) {
+		int price = calculatePrice();
 
-    public void decrement(View view) {
-        if (initialCoffeeQuanity > 1) {
-            initialCoffeeQuanity = initialCoffeeQuanity - 1;
-            displayQuanity(initialCoffeeQuanity);
-        }
-    }
+		CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.checkWhippedCream);
+		boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
 
-    /**
-     * This method is called when the order button is clicked.
-     */
+        CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.checkChocolate);
+        boolean hasChocolate = chocolateCheckbox.isChecked();
 
-    public void submitOrder(View view){
-        String priceMessage = String.valueOf(initialCoffeeQuanity * oneCoffeeCupPrice);
-        displayMessage(priceMessage);
-        //displayPrice(initialCoffeeQuanity * oneCoffeeCupPrice);
-    }
+        EditText summaryNameEditText = (EditText) findViewById(R.id.order_summary_name);
+        String editTextValue = summaryNameEditText.getText().toString();
 
-    /**
-     * This method displays the total price on the screen.
-     */
+		String priceMessage = createOrderSummary(price,hasWhippedCream,hasChocolate,editTextValue);
+		displayMessage(priceMessage);
+	}
 
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText("Total: " + "\u20B9" + " "+ message + "\nThank You!");
-    }
+	/**
+	 * This method is used to calculate coffee price.
+	 *
+	 * @return total price of the Coffee
+	 */
+	public int calculatePrice() {
+		return initialCoffeeQuantity * oneCoffeeCupPrice;
+	}
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
-    private void displayQuanity(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
-    }
+	/**
+	 * This method is used to generate order summary.
+	 */
+	public String createOrderSummary(int totalPrice, boolean hasWhippedCream,
+                                     boolean hasChocolate, String editTextValue) {
+		return "Name: " + editTextValue
+				+ "\nAdd whipped cream? " + hasWhippedCream
+                + "\nAdd chocolate? " + hasChocolate
+				+ "\nQunatity: " + initialCoffeeQuantity
+				+ "\nTotal: " + "\u20B9" + " " + totalPrice
+				+ "\nThank You!";
+	}
+
+	/**
+	 * This method displays the total price on the screen.
+	 */
+
+	private void displayMessage(String message) {
+		TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+		orderSummaryTextView.setText(message);
+	}
+
+	/**
+	 * This method displays the given quantity value on the screen.
+	 */
+	private void displayQuanity(int numberofCoffees) {
+		TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+		quantityTextView.setText("" + numberofCoffees);
+	}
 
 }
